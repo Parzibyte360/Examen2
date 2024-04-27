39811f4c965256ade9474851c4b61fd4998946ec
Bruno <brunosantospamo@gmail.com>
2024-04-26 21:06:21 -0500

    Agregacion de propiedades y clase producto para ejemplo real

--- Examen2/Clases/Pedidos.cs
+++ Examen2/Clases/Pedidos.cs
@@ -10,5 +10,20 @@ namespace Examen2.Clases
     {
         public int NumeroPedido { get; set; }
         public string NombreCliente { get; set; }
+        public List<Producto> Productos { get; set; }
+
+        public Pedidos(int numeroPedido, string nombreCliente, List<Producto> productos)
+        {
+            NumeroPedido = numeroPedido;
+            NombreCliente = nombreCliente;
+            Productos = productos;
+        }
+    }
+
+    //Para ejemplificar una situacion real
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
     }
 }
--- Examen2/Program.cs
+++ Examen2/Program.cs
@@ -9,10 +9,40 @@ class Program
 
         GestorPedidos gestorPedidos = new GestorPedidos();
 
-        Pedidos pedidoElectronica = new Pedidos();
+        Pedidos pedidoElectronica = new Pedidos(1, "ClienteElectronica", new List<Producto>
+        {
+            new Producto { Nombre = "Smartphone", Precio = 500 },
+            new Producto { Nombre = "Laptop", Precio = 1200 }
+        });
+
+        Console.WriteLine("Pedido de productos electrónicos:");
+        Console.WriteLine($"Número de pedido: {pedidoElectronica.NumeroPedido}");
+        Console.WriteLine($"Cliente: {pedidoElectronica.NombreCliente}");
+        Console.WriteLine("Productos:");
+        foreach (var producto in pedidoElectronica.Productos)
+        {
+            Console.WriteLine($"- {producto.Nombre}: ${producto.Precio}");
+        }
+
         gestorPedidos.EnviarPedidoSegunTipoProducto(pedidoElectronica, proveedorElectronica);
 
-        Pedidos pedidoRopa = new Pedidos();
+
+
+        Pedidos pedidoRopa = new Pedidos(2, "ClienteRopa", new List<Producto>
+        {
+            new Producto { Nombre = "Camisa", Precio = 40 },
+            new Producto { Nombre = "Pantalón", Precio = 60 }
+        });
+
+        Console.WriteLine("\nPedido de productos de ropa:");
+        Console.WriteLine($"Número de pedido: {pedidoRopa.NumeroPedido}");
+        Console.WriteLine($"Cliente: {pedidoRopa.NombreCliente}");
+        Console.WriteLine("Productos:");
+        foreach (var producto in pedidoRopa.Productos)
+        {
+            Console.WriteLine($"- {producto.Nombre}: ${producto.Precio}");
+        }
+
         gestorPedidos.EnviarPedidoSegunTipoProducto(pedidoRopa, proveedorRopa);
 
         Console.ReadLine();
